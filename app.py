0e59a4836e03bbb86962612e4cb771162ac44da1
Sayan Biswas <sayancyan@gmail.com>
2025-07-14 02:53:16 +0530

    added comments

--- app.py
+++ app.py
@@ -7,12 +7,7 @@ import google.generativeai as genai
 genai.configure(api_key=st.secrets["GOOGLE_API_KEY"])
 
 
-
-def get_gemini_response(input_prompt, pdf_content, job_description):
-    model = genai.GenerativeModel('gemini-1.5-flash')
-    response = model.generate_content([input_prompt, pdf_content[0], job_description])
-    return response.text
-
+# Getting Input
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
 
@@ -25,11 +20,10 @@ def input_pdf_setup(uploaded_file):
                 img_data = pix.tobytes("png")
                 images.append(img_data)
             return images
-
+        
+        ## main
         images = convert_pdf_to_images(uploaded_file)
-
         first_page = images[0]
-
         pdf_parts = [
             {
                 "mime_type": "image/png",
@@ -40,8 +34,13 @@ def input_pdf_setup(uploaded_file):
     else:
         raise FileNotFoundError("No file uploaded")
 
-## Streamlit App
+# Giving Output
+def get_gemini_response(input_prompt, pdf_content, job_description):
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    response = model.generate_content([input_prompt, pdf_content[0], job_description])
+    return response.text
 
+## Streamlit App Setup
 st.set_page_config(page_title="ATS Resume Expert")
 st.header("ATS Tracking System")
 input_text = st.text_area("Job Description: ", key="input")
@@ -50,22 +49,25 @@ uploaded_file = st.file_uploader("Upload your resume(PDF)...", type=["pdf"])
 if uploaded_file is not None:
     st.write("PDF Uploaded Successfully")
 
-submit1 = st.button("Tell Me About the Resume")
-
+# Buttons
+submit1 = st.button("Resume Overview")
 submit3 = st.button("Percentage match")
 
+# Input Prompts
 input_prompt1 = """
-You are an experienced Technical Human Resource Manager. Your task is to review the provided resume against the job description. 
+You are an experienced Technical Human Resource Manager.
+Your task is to review the provided resume against the job description. 
 Please share your professional evaluation on whether the candidate's profile aligns with the role. 
 Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
-
 input_prompt3 = """
-You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality. 
-Your task is to evaluate the resume against the provided job description. Give me the percentage of match if the resume matches
-the job description. First the output should come as percentage, then keywords missing, and last final thoughts.
+You are a skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality.
+Your task is to evaluate the resume against the provided job description.
+Give me the percentage of match if the resume matches the job description.
+First the output should come as percentage, then keywords missing, and last final thoughts.
 """
 
+# Input Config
 if submit1:
     if uploaded_file is not None:
         pdf_content = input_pdf_setup(uploaded_file)
@@ -74,7 +76,6 @@ if submit1:
         st.write(response)
     else:
         st.write("Please upload the resume")
-
 elif submit3:
     if uploaded_file is not None:
         pdf_content = input_pdf_setup(uploaded_file)
